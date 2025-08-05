11da61d717d3d6b2b270b59cf6d4f2d461e775a6
Jasmine Lai <jasmine.lai@chunhesoftware.com>
2025-08-05 14:54:03 +0800

    fix issue

--- src/main/java/com/amway/interview/Main.java
+++ src/main/java/com/amway/interview/Main.java
@@ -1,13 +1,13 @@
 package com.amway.interview;
 
 import com.amway.interview.calculator.Calculator;
+import com.amway.interview.calculator.Validator;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         double a = 10.0898132d;
         double b = 23.2489134d;
-        Calculator calculator = new Calculator();
+        Calculator calculator = new Calculator(new Validator());
         double addResult = calculator.add(a,b);
         System.out.println("result = " + calculator.displayResult(addResult));
         double substractResult = calculator.substract(a,b);
